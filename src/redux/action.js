22acabe8758ac7232efc9e31b4fa5f63844abf21
Zhangyao719 <1258858403@qq.com>
2022-10-03 20:39:19 +0800

    feat: redux-devtools

--- src/redux/action.js
+++ src/redux/action.js
@@ -5,4 +5,13 @@ export const addOne = payload => ({ type: TYPES.ADD_ONE, payload })
 export const subOne = payload => ({ type: TYPES.SUB_ONE, payload })
 export const addMore = payload => ({ type: TYPES.ADD_MORE, payload })
 export const subMore = payload => ({ type: TYPES.SUB_MORE, payload })
-export const setName = payload => ({ type: TYPES.SET_NAME, payload })
+
+export const setName = payload => {
+    // * 使用 redux-thunk 中间件后, 支持异步处理操作
+    // * 返回 一个包含 dispatch 的函数
+    return (dispatch, getState) => {
+        setTimeout(() => {
+            dispatch({ type: TYPES.SET_NAME, payload })
+        }, 1000)
+    }
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -7,8 +7,13 @@
 
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from '@redux-devtools/extension' //redux devtool 开发工具的配置 (在中间件外面包裹)
 import reducer from './reducer'
 
-const store = createStore(reducer, applyMiddleware(logger))
+const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(thunk, logger))
+)
 
 export default store
